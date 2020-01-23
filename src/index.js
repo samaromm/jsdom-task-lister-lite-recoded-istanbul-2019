31716c9dddd34a79ac1800a1789829a0814edb89
Samar Omar <samaromm98@gmail.com>
2020-01-23 21:27:13 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let TaskForm = document.getElementById("create-task-form");
+  TaskForm.addEventListener('submit',(e) =>{
+    e.preventDefault();
+    let item = document.getElementById('new-task-description').value;
+    
+    let del= document.createElement('span');
+    let ex = document.createTextNode('\u00D7');
+    del.appendChild(ex);
+    del.className='close';
+    
+    var forLi= document.createElement('li');
+    forLi.textContent=item+'      ';
+    forLi.appendChild(del);
+    document.getElementById('tasks').appendChild(forLi);
+    
+    let close = document.getElementsByClassName("close");
+    for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+    let div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+    });
 });
