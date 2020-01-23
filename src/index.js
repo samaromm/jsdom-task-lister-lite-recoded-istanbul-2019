1b38b2bcd42ac65e956f1b8aa51649ec740ed44f
Samar Omar <samaromm98@gmail.com>
2020-01-23 21:10:43 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
   TaskForm.addEventListener('submit',(e) =>{
     e.preventDefault();
     let item = document.getElementById('new-task-description').value;
+    
     let del= document.createElement('span');
+    let ex = document.createTextNode()
     del.textContent="\u00D7";
     del.className='close';
+    
     var forLi= document.createElement('li');
     forLi.textContent=item+'      ';
     forLi.appendChild(del);
     document.getElementById('tasks').appendChild(forLi);
+    
     for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
       let div = this.parentElement;
