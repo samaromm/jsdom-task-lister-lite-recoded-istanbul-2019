eb795247f4fe78faff51d9e59c789adc79069c79
Samar Omar <samaromm98@gmail.com>
2020-01-23 20:57:00 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let TaskForm = document.getElementById("create-task-form");
   TaskForm.addEventListener('submit',function todo(){
+    TaskForm.
     let item = document.getElementById('new-task-description').value;
     var text= document.createElement('li').innerHTML=item;
     document.getElementById('tasks').appendChild(text);
