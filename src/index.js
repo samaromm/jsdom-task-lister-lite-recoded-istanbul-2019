d6728c222325c9cf0811759d61f05186b03497dd
Samar Omar <samaromm98@gmail.com>
2020-01-23 20:44:42 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   function todo(){
     let item = document.getElementById('new-task-description').value;
-    var text= document.createElement(item)
+    var text= document.createElement(item);
   }
 });
