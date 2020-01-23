b78fa3aee0e8840663e544d98d9ff5e616aed3f3
Samar Omar <samaromm98@gmail.com>
2020-01-23 21:13:11 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", () => {
     forLi.appendChild(del);
     document.getElementById('tasks').appendChild(forLi);
     
-    
+    let close = document.getElementsByClassName("close");
+    for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+    let div = this.parentElement;
+    div.style.display = "none";
+  }
+}
     });
 });
